9456e75093c2a7d80e531e21dfa2ce57b81df226
ybeyka <122684432+ybeyka@users.noreply.github.com>
2023-06-06 06:14:42 +0300

    ss

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int val = 34786;
+
+int v1 = val / 10000;
+Console.WriteLine(v1);
+int v2 = (val - v1 * 10000) / 1000;
+Console.WriteLine(v2);
+int v3 = (val - (v1 * 10000) - (v2 * 1000)) / 100;
+Console.WriteLine(v3);
+int v4 = (val - (v1 * 10000) - (v2 * 1000) - (v3 * 100)) / 10;
+Console.WriteLine(v4);
+int v5 = val - (v1 * 10000) - (v2 * 1000) - (v3 * 100) - (v4 * 10);
+Console.WriteLine(v5);
+
